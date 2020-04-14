253db8a0b5dc0d6a45556931ea43fa8b732a6d24
J0hn <j0hntvdesign@gmail.com>
2020-04-14 12:52:52 +0300

    Add product to cart

--- moltin.py
+++ moltin.py
@@ -8,8 +8,25 @@ def get_products(token):
     response.raise_for_status()
     return response.json()['data']
 
-def add_product_to_cart():
-    pass
+
+def add_product_to_cart(token, user_id, product_id, quantity):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        "data": {
+            "id": product_id,
+            "type": "cart_item",
+            "quantity": quantity
+        }
+    }
+
+    url = f'https://api.moltin.com/v2/carts/{user_id}/items'
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    return response.json()['data']
+
 
 def get_a_cart():
     pass
