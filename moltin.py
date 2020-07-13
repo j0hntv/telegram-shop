16dafb7f47f30cac50812f01250402310b7e8e7c
J0hn <j0hntvdesign@gmail.com>
2020-07-13 15:41:18 +0300

    Fix naming

--- moltin.py
+++ moltin.py
@@ -26,7 +26,7 @@ def get_products(token, product_id=None):
     return response.json()['data']
 
 
-def add_product_to_cart(token, user_id, product_id, quantity):
+def add_product_to_cart(token, cart, product_id, quantity):
     headers = {
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
@@ -39,31 +39,31 @@ def add_product_to_cart(token, user_id, product_id, quantity):
         }
     }
 
-    url = f'https://api.moltin.com/v2/carts/{user_id}/items'
+    url = f'https://api.moltin.com/v2/carts/{cart}/items'
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
     return response.json()['data']
 
 
-def get_a_cart(token, user_id):
+def get_a_cart(token, cart):
     headers = {'Authorization': f'Bearer {token}'}
-    url = f'https://api.moltin.com/v2/carts/{user_id}'
+    url = f'https://api.moltin.com/v2/carts/{cart}'
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()['data']
 
 
-def get_cart_items(token, user_id):
+def get_cart_items(token, cart):
     headers = {'Authorization': f'Bearer {token}'}
-    url = f'https://api.moltin.com/v2/carts/{user_id}/items'
+    url = f'https://api.moltin.com/v2/carts/{cart}/items'
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()['data']
 
 
-def remove_cart_item(token, user_id, product_id):
+def remove_cart_item(token, cart, product_id):
     headers = {'Authorization': f'Bearer {token}'}
-    url = f'https://api.moltin.com/v2/carts/{user_id}/items/{product_id}'
+    url = f'https://api.moltin.com/v2/carts/{cart}/items/{product_id}'
     response = requests.delete(url, headers=headers)
     response.raise_for_status()
     return response.json()
